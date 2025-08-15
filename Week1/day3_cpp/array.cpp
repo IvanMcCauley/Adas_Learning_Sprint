4ccfe8dc2c8df6e8a1a6eb1743e709992cddc39e
IvanMcCauley <mccauleyivan03@gmail.com>
2025-08-15 18:24:10 +0100

    Day3: arrays — read 5 ints, sum + max

--- Week1/day3_cpp/array.cpp
+++ Week1/day3_cpp/array.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main(){
     int arr[5];
-    cout << "Enter 5 numbers:\n";
-    for (int i=0; i<5; ++i){
-        cin >> arr[i];
+    cout << "Enter 5 numbers:\n"; //PROMPT
+    for (int i=0; i<5; ++i){ 
+        cin >> arr[i];// ALLOWS USER TO TYPE IN 5 NUMBERS
     }
-    int sum = 0;
-    int maxv = arr[0];
+    int sum = 0; //INITIALIZES SUM
+    int maxv = arr[0]; //INITIALIZES MAX AS INDEX 0
     for (int i = 0; i<5; ++i){
-        sum += arr[i];
-        if (arr[i] > maxv) maxv = arr[i];
+        sum += arr[i];   // ITERATES THROIGH THE ARRAY AND ADDS EACH NUMBER ONTO IT AS A RUNNING SUM
+        if (arr[i] > maxv) maxv = arr[i];// ITERATES THROUGH THE ARRAY AND REPLACES MAXV WITH A NUMBER IN THE ARRAY IF ITS BIGGER THAN THE CURRENT MAXV
     }    
     cout << "Sum = " << sum << "\n";
     cout << "Max: " << maxv << "\n";
