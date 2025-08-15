934bdba338bf974f3e55734b78667a8a19580e0e
IvanMcCauley <mccauleyivan03@gmail.com>
2025-08-15 17:39:59 +0100

    Day3: loops (for/while/do-while) + factorial & sum; cin validation demo

--- Week1/day3_cpp/loops.cpp
+++ Week1/day3_cpp/loops.cpp
@@ -3,11 +3,41 @@ using namespace std;
 
 int main(){
     int i = 1;
+    for (int i =1; i <=10; ++i) {
+        cout << i << " ";
+    }
+    cout << "\n";
+
     while (i <= 10) {
         cout << i << " ";
         ++i;
     }
+
     cout << "\n";
+
+    int k =1;
+    do {
+        cout << k << " "; 
+        ++k;
+     } while (k <= 10);
+   
+    cout << "\n";
+
+    cout << "Type an integer: ";
+    int n;
+    cin >> n;
+    
+    long long fact = 1;
+    for (int t =2; t<=n; ++t){
+        fact *= t;
+    }
+    cout << n << "! = " << fact << "\n";
+
+    long long sum = 0;
+    for (int t = 1; t <=n; ++t) {
+        sum +=t;
+    }
+    cout << "Sum 1.." << n << " = " << sum << "\n";
     return 0;
 }
    
