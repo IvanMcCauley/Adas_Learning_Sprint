9e1990b999f32dd8660dca8de4559c439cb8df7b
IvanMcCauley <mccauleyivan03@gmail.com>
2025-08-15 22:22:56 +0100

    Day3: non static variables

---
+++ Week1/day3_cpp/puzzles.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+int a = 3;
+
+if(a == 5){
+    cout << "a is five\n";
+} else {
+    cout << "a is not 5\n";
+}
+cout << "a = " << a << "\n";
+return 0;
+}
